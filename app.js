88584c92935604c35cab19f87e218e553c8f9274
gkttiw98-ai <gkttiw98@gmail.com>
2025-11-20 18:46:01 +0530

    Update app.js cors

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ import cookieParser from "cookie-parser";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const app = express();
-app.use(cookieParser());
+ app.use(cookieParser());
 
 // Middlewares
 app.use(express.json());
