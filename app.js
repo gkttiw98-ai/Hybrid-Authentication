8e45821c5cebe7a8361ccb0e93bf7d2160463e2d
gkttiw98-ai <gkttiw98@gmail.com>
2025-11-20 18:44:26 +0530

    Update cors second time

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 // Middlewares
 app.use(express.json());
 app.use(cors({
-    origin: "https://hybrid-authentication.onrender.com/",  // your frontend port, change if needed
+    origin: "https://hybrid-authentication.onrender.com",  // your frontend port, change if needed
     credentials: true
 }));
 
